ca30a8309e84aba8b804055c01d607aaaa3a82f9
lx-aliaksandrsemchanka <aliaksandr.semchanka@leverx.com>
2023-02-28 14:05:12 +0300

    start script added

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
 const app = express();
+
+const TelegramBot = require('node-telegram-bot-api');
+
+const API_KEY = '6255590542:AAGuf6H3YLF94gdStmr2XA9y2ELv2Ws5A90';
+const chatId = '383803334';
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
